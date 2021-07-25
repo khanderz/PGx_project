6f3a9c1cb53e584529551b8a531689efee926282
Khanh Mai <khanhmai@creighton.edu>
2021-07-25 23:14:26 +0000

    successful API transfer of flowchart to index.html

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 from flask import (Flask, render_template, request)
 from jinja2 import StrictUndefined
+import requests
 
 
 app = Flask(__name__)
@@ -11,10 +12,21 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def homepage():
     """View homepage."""
-  
-  return render_template('index.html')
+    drug_name = "celecoxib"
+
+    url = 'https://data.cpicpgx.org/v1/drug?name=in.("' + drug_name + '")'
+
+
+    res = requests.get(url)
+    data = res.json()
+    flowchart = data[0]['flowchart']
+
+    print(flowchart, "****************************RESULTS")
+
+
+    return render_template('index.html', 
+                            flowchart=flowchart)
 
 
 if __name__ == '__main__':
-    connect_to_db(app)
     app.run(host='0.0.0.0', debug=True)
