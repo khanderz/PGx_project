1962db5f7fce19bce10818fa1eceaa3ea4c84a7c
Khanh Mai <khanhmai@creighton.edu>
2021-07-27 20:17:32 +0000

    fda search implemented

--- server.py
+++ server.py
@@ -3,11 +3,14 @@
 from flask import (Flask, render_template, request)
 from jinja2 import StrictUndefined
 import requests
+import os
 
 
 app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
+
+API_KEY = os.environ['FDA_KEY']
 
 @app.route('/')
 def homepage():
@@ -17,7 +20,7 @@ def homepage():
 
 @app.route('/load_options')
 def load_options():
-    """load gene symbols onto option list"""    
+    """load gene symbols and results onto option list"""    
 
     url = 'https://data.cpicpgx.org/v1/gene_result'
     res = requests.get(url)
@@ -38,24 +41,26 @@ def load_options():
         # to access name: data[i]['key']
 
 
-    drug_gene_symbol = []
+    gene_symbol = set([])
+    results = set([])
 
     for key in data:
-       drug_gene_symbol.append(key['genesymbol'])
+       gene_symbol.add(key['genesymbol'])
+       results.add(key['result'])
 
-  
 
-    return render_template('index.html', drug_gene_symbol=drug_gene_symbol)
+    return render_template('index.html', gene_symbol=gene_symbol, results=results)
 
 
 @app.route('/results')
 def render_results():
-    """View results page"""
+    """render custom search query on CPIC API"""
 
     gene = request.args.get('gene')
-    print(gene, "****************************gene name")
+    result = request.args.get('result')
+    print(gene, result, "^^^^^^^^^^^^^^^gene and result")
 
-    url = 'https://data.cpicpgx.org/v1/gene_result?genesymbol=in.("' + gene + '")'
+    url = 'https://data.cpicpgx.org/v1/gene_result?genesymbol=in.("' + gene + '")&result=in.("' + result + '")'
 
     res = requests.get(url)
     data = res.json()
@@ -67,6 +72,57 @@ def render_results():
     return render_template('results.html', 
                             cds=cds)
 
+@app.route('/pharmgkb_id')
+def retrieve_pharmgkb_id():
+    """get pharmgkbid from CPIC API"""
+
+    url = 'https://data.cpicpgx.org/v1/drug'
+    res = requests.get(url)
+    data = res.json()
+
+    drugs = []
+
+    for key in data:
+        drugs.append(key['name'])
+
+    return render_template('pharmgkb_search.html', drugs=drugs)    
+
+@app.route('/pharmgkb_search')
+def query_pharmgkb():
+    drug = request.args.get('drug')
+    print(drug, "****************************drug")
+
+    url = 'https://data.cpicpgx.org/v1/drug?name=in.("' + drug + '")'
+    res = requests.get(url)
+    data = res.json()
+    print(data, "****************************data")
+
+    pharmgkb_id = data[0]['pharmgkbid']
+    print(pharmgkb_id, "****************************pharmgkb ID")
+
+    url = "https://api.pharmgkb.org/v1/data/guideline?relatedChemicals.accessionId=" + pharmgkb_id 
+    print(url, "****************************url")
+    res = requests.get(url)
+    print(res, "****************************res")
+
+    data = res.json()
+
+
+    return render_template('pharmgkb_results.html', data=data)
+
+@app.route('/fda_search')    
+def query_fda_for_dosing():
+    drug = "aripiprazole"
+
+    url = 'https://api.fda.gov/drug/label.json?api_key=' + API_KEY + '&search=' + drug
+
+    print(url, "****************************url")
+    res = requests.get(url)
+    print(res, "****************************res")
+
+    data = res.json()
+
+    return render_template('fda_results.html', data=data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
